25bcc1601922dab55232158852de9d6e74f04127
Muneer Ahmed Babar <babarmuneerahmed789786@gmail.com>
2026-09-11 01:05:55 +0500

    Add express-session setup

--- classroom/server.js
+++ classroom/server.js
@@ -2,38 +2,50 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
-const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
-app.use(cookieParser("secretcode"));
+// Express Sessions
+app.use(session({ secret: "mysupersecretstring" }));
 
-app.get("/getsignedcookie", (req, res) => {
-  res.cookie("made-in", "Pakistan", { signed: true });
-  res.send("signed cookie sent");
+app.get("/test", (req, res) => {
+  res.send("test successful!");
 });
 
-app.get("/verify", (req, res) => {
-  console.log(req.signedCookies);
-  res.send("verified");
-});
 
-app.get("/getcookies", (req, res) => {
-  res.cookie("greet", "assalam-o-alaikum");
-  res.cookie("madeIn", "Pakistan");
-  res.send("sent you some cookies!");
-});
 
-app.get("/greet", (req, res) => {
-  let { name = "anonymous" } = req.cookies;
-  res.send(`Hi, ${name}`);
-});
+// const cookieParser = require("cookie-parser");
+// app.use(cookieParser("secretcode"));
 
-app.get("/", (req, res) => {
-  console.dir(req.cookies);
-  res.send("Hi, I am root!");
-});
+// Cookie-Parser
 
-app.use("/users", users);
-app.use("/posts", posts);
+// app.get("/getsignedcookie", (req, res) => {
+//   res.cookie("made-in", "Pakistan", { signed: true });
+//   res.send("signed cookie sent");
+// });
+
+// app.get("/verify", (req, res) => {
+//   console.log(req.signedCookies);
+//   res.send("verified");
+// });
+
+// app.get("/getcookies", (req, res) => {
+//   res.cookie("greet", "assalam-o-alaikum");
+//   res.cookie("madeIn", "Pakistan");
+//   res.send("sent you some cookies!");
+// });
+
+// app.get("/greet", (req, res) => {
+//   let { name = "anonymous" } = req.cookies;
+//   res.send(`Hi, ${name}`);
+// });
+
+// app.get("/", (req, res) => {
+//   console.dir(req.cookies);
+//   res.send("Hi, I am root!");
+// });
+
+// app.use("/users", users);
+// app.use("/posts", posts);
 
 app.listen(3000, () => {
   console.log("server is listening to 3000");
